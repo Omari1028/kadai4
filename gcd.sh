99f29751406a9df401c1ae153197aeacc3a0e082
Omari1028 <67819263+Omari1028@users.noreply.github.com>
2020-07-05 19:27:02 +0900

    Update gcd.sh

--- gcd.sh
+++ gcd.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
 
+# Script for finding gcd of two number 
+# echo is for printing the message   
+echo Enter two numbers with space in between 
+  
+# read for scanning 
+read a b 
+  
+# Assigning the value of a to m 
+m = $a 
+  
+# Condition checking if b greater than m 
+# If yes the replace the value of m assign a new value 
+if [ $b -lt $m ] 
+then 
+m = $b 
+fi 
+  
+# In do while  loop we are checking the gcd 
+while [ $m -ne 0 ] 
+do
+x = `expr $a % $m` 
+y = `expr $b % $m` 
+  
+# If x and y both are 0 then we complete over  
+# process and we print  the gcd 
+if [ $x -eq 0 -a $y -eq 0 ] 
+then 
+  
+# Printing the greatest gcd of two given number 
+echo gcd of $a and $b is $m 
+break
+fi 
+m = `expr $m - 1` 
+  
+done 
