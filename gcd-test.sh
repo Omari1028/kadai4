a542434eb3932c09f07834c64760e6f221712709
Omari1028 <67819263+Omari1028@users.noreply.github.com>
2020-07-05 23:58:16 +0900

    Update gcd-test.sh

--- gcd-test.sh
+++ gcd-test.sh
@@ -3,5 +3,5 @@
 echo hello
 
 ./gcd.sh > /tmp/$$-result
-echo "input two numbers with space" > /tmp/$$-ans
-diff /tmp/$$-ans /tmp/$$-result && echo "OK" || echo "NG"
+echo "input two numbers when you enter gcd.sh" > /tmp/$$-ans
+diff /tmp/$$-ans /tmp/$$-result && echo "NG" || echo "OK"
