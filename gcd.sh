e0b89623b8dc028640f1186c5452d9165e910539
Omari1028 <67819263+Omari1028@users.noreply.github.com>
2020-07-05 19:46:24 +0900

    Update gcd.sh

--- gcd.sh
+++ gcd.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
- 
-echo "Enter two numbers with pace"
 
-read a b 
+m=$1         # dividend
+n=$2          # divisor
+r=1            # remainder
 
-m = $a   
-# Condition checking if b greater than m 
-# If yes the replace the value of m assign a new value 
-if [ $b -lt $m ] 
-then 
-m = $b 
-fi 
-  
-# In do while  loop we are checking the gcd 
-while [ $m -ne 0 ] 
+until [ $r == 0 ]
 do
-x = `expr $a % $m` 
-y = `expr $b % $m` 
-  
-# If x and y both are 0 then we complete over  
-# process and we print  the gcd 
-if [ $x -eq 0 -a $y -eq 0 ] 
-then 
-  
-# Printing the greatest gcd of two given number 
-echo gcd of $a and $b is $m 
-break
-fi 
-m = `expr $m - 1` 
-done 
+   let "r =$m % $n"
+   m=$n
+   n=$r
+done
+
+echo "The GCD of $1 and $2 is $m"
