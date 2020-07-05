6ffce404bfa638a5881a3d7b8fc27227d36472f1
Omari1028 <67819263+Omari1028@users.noreply.github.com>
2020-07-05 19:57:10 +0900

    Update gcd.sh

--- gcd.sh
+++ gcd.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-echo "Entor two numbers with space."
-
 if [[ $1 -lt $2 ]]; then
 m=$2
 n=$1
 else
 m=$1         # dividend
 n=$2          # divisor
-r=1            # remainder
 fi
+r=1            # remainder
 
 until [ $r == 0 ]
 do
