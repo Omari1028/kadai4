94ece3f8a472fdfd4b3e0f500e5876f5b5d5555d
Omari1028 <67819263+Omari1028@users.noreply.github.com>
2020-07-05 19:55:19 +0900

    Update gcd.sh
    大きい方の数字

--- gcd.sh
+++ gcd.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo "Entor two numbers with space."
+
+if [[ $1 -lt $2 ]]; then
+m=$2
+n=$1
+else
 m=$1         # dividend
 n=$2          # divisor
 r=1            # remainder
+fi
 
 until [ $r == 0 ]
 do
