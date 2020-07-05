ea6a2e807db26b46e18239ab7bdfc23c3247605b
Omari1028 <67819263+Omari1028@users.noreply.github.com>
2020-07-06 00:02:37 +0900

    Update gcd-test.sh

--- gcd-test.sh
+++ gcd-test.sh
@@ -4,4 +4,10 @@
 
 echo "input two numbers when you enter gcd.sh" > /tmp/$$-ans
 
-diff /tmp/$$-ans /tmp/$$-result && echo "NG" || echo "OK"
+diff /tmp/$$-ans /tmp/$$-result && echo "check1 OK" || echo "check1 NG"
+
+./gcd.sh 66 88 > /tmp/$$-result
+
+echo "The GCD of 66 and 88 is 22" > /tmp/$$-ans
+
+diff /tmp/$$-ans /tmp/$$-result && echo "check2 OK" || echo "check2 NG"
