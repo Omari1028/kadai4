3bd6212c2bb4fdd4346bb6175f17de29d4e74d7b
Omari1028 <67819263+Omari1028@users.noreply.github.com>
2020-07-05 23:11:09 +0900

    Update gcd-test.sh

--- gcd-test.sh
+++ gcd-test.sh
@@ -2,14 +2,5 @@
 
 tmp=/home/mohara/kadai4/tmp
 
-ERROR_EXIT () { # ERROR関数
-echo "$1" >&2 # エラーメッセージ(引数1)を標準エラー出⼒に表⽰
-rm -f tmp/$$-* # 作ったファイルの削除
-exit 1 # エラー終了
-}
-
 ./gcd.sh > tmp/$$-result # 結果データをresult-$$に
 
-diff tmp/$$-ans tmp/$$-result || ERROR_EXIT "result err" # 失敗
-
-rm -f tmp/$$-* # 後始末
